c90008dbc178911057ac53dcd638edb7b65f7cb3
Yogi Hafidh Maulana <100673796+Yogihafidh@users.noreply.github.com>
2025-05-14 15:11:57 +0700

    feat: add dataStore.js

---
+++ models/dataStore.js
@@ -0,0 +1,33 @@
+module.exports = {
+  products: [
+    {
+      id: "1",
+      name: "Sabun Daya",
+      price: 12000,
+      category: "Sabun",
+      stock: 10,
+    },
+    {
+      id: "2",
+      name: "Sabun Lifeboy",
+      price: 5000,
+      category: "Sabun",
+      stock: 25,
+    },
+    {
+      id: "3",
+      name: "Galon Lemineral",
+      price: 1,
+      category: "Sembako",
+      stock: 100,
+    },
+  ],
+  transactions: [
+    {
+      id: "1001",
+      productId: "1",
+      quantity: 1,
+      total: 900,
+    },
+  ],
+};
